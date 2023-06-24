3f1a94d937cdafd2fa96ba1d09218bd3cf99ae24
zero-goliath <goliath@ritser.com>
2023-06-24 19:03:28 -0400

    add logic

--- components/page-component.tsx
+++ components/page-component.tsx
@@ -1,10 +1,30 @@
-import {Input, Text, VStack} from "@chakra-ui/react";
+import { getLenderProfitGraph } from "@/src/query";
+import {Input, Text, VStack, Button, Textarea} from "@chakra-ui/react";
+import {useState, useEffect} from "react";
 
 export const PageComponent = () => {
+    const [account, setAccount] = useState<string>('')
+    const [textBox, setTextBox] = useState<string>('')
+
+    const buttonCallback = async () => {
+        setTextBox('Fetching data...')
+        const result = await getLenderProfitGraph(account, 18);
+    }
+
     return  (
         <VStack>
-            <Text>fdglks</Text>
-            <Input></Input>
+            <Text>Have I been liquidated?</Text>
+            <Input
+                placeholder="Enter wallet address"
+                value={account}
+                onChange={(e) => setAccount(e.target.value)}
+            ></Input>
+            <Button onClick={buttonCallback}>Check</Button>
+            <Text></Text>
+            <Textarea
+                value={textBox}
+                onChange={(e) => setTextBox(e.target.value)}
+            ></Textarea>
         </VStack>
     )
 }
