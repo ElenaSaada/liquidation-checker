d89cd41a9558ca216cf8c9f958bf31dbc436ca9b
zero-goliath <goliath@ritser.com>
2023-06-24 19:33:47 -0400

    fix query.ts

--- src/query.ts
+++ src/query.ts
@@ -5,7 +5,7 @@ const API_URL = 'https://api.thegraph.com/subgraphs/name/yama-finance/yama-finan
 const auctionQuery = `
     query($vaultOwner: Bytes) {
         auctions(first: 1000, where: { vault__owner: $vaultOwner }) {
-            initialPrice
+            startPrice
             startTime
         }
     }
@@ -28,7 +28,7 @@ export const getLenderProfitGraph = async (
         const { data } = await client.query({
             query: gql(auctionQuery),
             variables: {
-                id: account,
+                vaultOwner: account,
             },
         });
         let result = '';
@@ -37,14 +37,14 @@ export const getLenderProfitGraph = async (
             return 'No liquidations found';
         }
         auctions.forEach(auction => {
-            const initialPrice = auction.initialPrice / 10 ** decimals;
+            const startPrice = auction.startPrice / 10 ** decimals;
             const startTime = auction.startTime;
             const formattedTime = getFormattedTimeString(startTime);
-            result += `${formattedTime} - liquidation auction starting at ${initialPrice}\n`;
+            result += `${formattedTime} - liquidation auction starting at ${startPrice}\n`;
         });
         return result;
     } catch (error) {
         console.log(error);
-        return 'Error fetching data';
+        return 'Error fetching data: \n' + error.message;
     }
 }
