ef6e9f7de03e8c8eea6f8f0924b5ad93c3acd7aa
ElenaSaada <saadaelena@gmail.com>
2023-06-24 18:55:03 -0400

    commit

---
+++ components/page-component.tsx
@@ -0,0 +1,10 @@
+import {Input, Text, VStack} from "@chakra-ui/react";
+
+export const PageComponent = () => {
+    return  (
+        <VStack>
+            <Text>fdglks</Text>
+            <Input></Input>
+        </VStack>
+    )
+}
